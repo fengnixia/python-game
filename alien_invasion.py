5f6755dcff166f55331f822ef83fee137da75c34
fengnixia <banruoyun0311@163.com>
2018-12-22 17:44:10 +0800

    外星人入侵

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,9 +4,13 @@
 #
 # December 2018
 import pygame
+from pygame.sprite import Group
 
 import game_functions as gf
+from button import Button
 from contants import settings
+from game_stats import GameStats
+from score_board import Scoreboard
 from ship import Ship
 
 
@@ -14,15 +18,33 @@ def run_game():
     # initialize game and create a dispaly object
     pygame.init()
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
-    ship = Ship(settings, screen)
     pygame.display.set_caption("Alien Invasion")
 
-    # game loop
+    # Make the Play button.
+    play_button = Button(screen, "Play")
+
+    # Create an instance to store game statistics, and a scoreboard.
+    stats = GameStats(settings)
+    sb = Scoreboard(settings, screen, stats)
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
     while True:
-        # supervise keyboard and mouse item
-        gf.check_events(ship)
-        gf.update_screen(settings, screen, ship)
-        ship.update()
+        gf.check_events(settings, screen, stats, sb, play_button, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(settings, screen, stats, sb, ship, aliens, bullets)
+
+        gf.update_screen(settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -4,12 +4,14 @@
 #
 # December 2018
 import pygame
+from pygame.sprite import Sprite
 
 
-class Ship():
+class Ship(Sprite):
 
     def __init__(self, settings, screen):
         # initialize spaceship and its location
+        super(Ship, self).__init__()
         self.screen = screen
         self.settings = settings
 
@@ -21,8 +23,17 @@ class Ship():
         # put spaceship on the bottom of window
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags.
         self.moving_right = False
         self.moving_left = False
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
 
     def update(self):
         if self.moving_right:
